7703637a07db0fcc8236d4450e55fc78614cb282
KLA6 <jay.casee@gmail.com>
2022-03-19 09:45:12 +0000

    0.2.1

--- src/MoreLaravelController.php
+++ src/MoreLaravelController.php
@@ -13,9 +13,10 @@ class MoreLaravelController extends Controller {
    * Get a SVG file content from `config( 'k6_more_function.icon_path' )` to return with attributes.
    */
   static function icon( $code = 'k6_logo', $width = 16, $height = 16, $class = '', $style = '', $attr = '' ) {
-    $path = config            ( 'k6_more_laravel.icon_path'                                                                      );
-    $svg  = file_get_contents ( "$path/icon/$code.svg"                                                                           );
-    return  str_replace       ( '<svg', "<svg width=\"$width\" height=\"$height\" class=\"$class\" style=\"$style\" $attr", $svg );
+    $path = config            ( 'k6_more_laravel.icon_path'                                                                        );
+    $svg  = file_get_contents ( "$path/icon/$code.svg"                                                                             );
+    $svg  = trim( preg_replace( '/<\?[^\?>]*\?\>/', ''                                                                    , $svg ) ); # XML 태그 제거
+    return         str_replace( '<svg', "<svg width=\"$width\" height=\"$height\" class=\"$class\" style=\"$style\" $attr", $svg   ); # SVG 속성 삽입
   } # function
 
 } # class
